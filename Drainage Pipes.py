194082c2b6360b0b576abfd695e3808e6b8119fa
Starlord82 <zvikadov@gmail.com>
2021-07-21 16:00:24 +0300

    Update Drainage Pipes.py

--- Drainage Pipes.py
+++ Drainage Pipes.py
@@ -1,6 +1,11 @@
 #Pipes
-class ClassName(object):
+class Pipe():
     """docstring for ClassName"""
-    def __init__(self, arg):
-        super(ClassName, self).__init__()
-        self.arg = arg
+    def __init__(self, diameter, length, maxdepth):
+        self.diameter = diameter
+        self.length = length
+        self.maxdepth = maxdepth
+
+    def twidth():
+        widths = {40:7.5,50:8.5,60:10,80:12.4,100:14.8,125:17.5,150:18}
+        
