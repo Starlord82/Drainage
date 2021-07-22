e003b80116f8e6abb62df56159079a145bdce42f
Starlord82 <zvikadov@gmail.com>
2021-07-22 08:32:05 +0300

    Update Prices.py

--- Prices.py
+++ Prices.py
@@ -5,5 +5,6 @@ from tkinter.filedialog import askopenfilename
 
 
 # df = tabula.read_pdf("Y:\dwg\משרד שיכון\מחירון\documents_tichnun_ironi_infrastructure_and_paving_price_list.pdf", pages='all')
-
-tabula.convert_into("Y:\dwg\משרד שיכון\מחירון\documents_tichnun_ironi_infrastructure_and_paving_price_list.pdf", "output.csv", output_format="csv", pages'=['7:]')
+filename = askopenfilename()
+tabula.convert_into(filename, filename +'.csv", output_format="csv", pages=[74,75,76])
+print('Finished')
