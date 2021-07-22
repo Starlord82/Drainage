44efd85110ff66ab27f9f043fc5a45e372afa747
Starlord82 <zvikadov@gmail.com>
2021-07-22 15:16:25 +0300

    Drainage Pipes update, Prices update

--- Drainage Pipes.py
+++ Drainage Pipes.py
@@ -1,11 +1,25 @@
 #Pipes
 class Pipe():
     """docstring for ClassName"""
-    def __init__(self, diameter, length, maxdepth):
+    dia_list = [40,50,60,80,100,125,150,180,200,240,320]
+    def __init__(self, name, diameter, length, maxdepth):
+        self.name = name
         self.diameter = diameter
+        while True:
+            if self.diameter in [40,50,60,80,100,125,150,180,200,240,320]:
+                break
+            else:
+                self.diameter = input('You have entered a bad diameter for pipe: '+ self.name + '\nplease enter a new diameter from the list:\n''40,50,60,80,100,125,150,180,200,240,320\n')
         self.length = length
         self.maxdepth = maxdepth
 
     def twidth():
-        widths = {40:7.5,50:8.5,60:10,80:12.4,100:14.8,125:17.5,150:18}
+        widths = {40:7.5,50:8.5,60:10,80:12.4,100:14.8,125:17.5,150:18,180:20,200:20,24:240,320:30}
+        return widths[diameter]
+
+pipe1 = Pipe('p-1',40,130,2.5)
+
+v = input('Whats you name:')
+print(v)
+
         
--- Prices.py
+++ Prices.py
@@ -6,5 +6,6 @@ from tkinter.filedialog import askopenfilename
 
 # df = tabula.read_pdf("Y:\dwg\משרד שיכון\מחירון\documents_tichnun_ironi_infrastructure_and_paving_price_list.pdf", pages='all')
 filename = askopenfilename()
-tabula.convert_into(filename, filename +'.csv", output_format="csv", pages=[74,75,76])
+tabula.io.convert_into(filename, 'output.csv', pages = '74-76')
+
 print('Finished')
