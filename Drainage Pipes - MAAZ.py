9377c963a8fa83a5883110c30878966068d19510
Starlord82 <87604535+Starlord82@users.noreply.github.com>
2022-10-25 15:40:22 +0300

    לפי מחירון נת"י

---
+++ Drainage Pipes - MAAZ.py
@@ -0,0 +1,104 @@
+#Quantity calculation from pipe csv exported from Civil3D
+#Author: Zvika Dov
+
+#The program takes a Pipe.csv file as an input, and returns an Excel file as an output
+# which organizes the data and sorts the quantities by diameter, depth and length.
+
+
+import pandas as pd
+from tkinter.filedialog import askopenfilename
+import numpy as np
+
+#Pipes
+dia_list = [40,50,60,80,100,125,150,180,200,240,320]
+widths = {40:7.5,50:8.5,60:10,80:12.4,100:14.8,125:17.5,150:18,180:20,200:20,24:240,320:30}
+class Pipe():
+    """docstring for ClassName"""
+    
+    def __init__(self, name, diameter, length):
+        self.name = name      
+        self.diameter = diameter
+        self.length = length
+        self.twidth = widths[int(self.diameter)]
+        self.depth = None
+
+    def __repr__(self):
+        return self.name
+        
+
+def convert_to_object_list(df,row):
+    '''returns tuples of values from dataframe'''
+    t = (df.loc[row,' Pipe Name'], df.loc[row,'Diameter'], df.loc[row,'Length'], df.loc[row,'Depth'])
+    return t
+
+def delete_level(df_input):
+    '''removes first level'''
+    df_input = df_input.rename(columns = df_input.iloc[0])
+    df_input = df_input.drop(0)
+    df_input = df_input.reset_index(drop = True)
+    return df_input
+
+def show_only_max_cover(df,row):
+    '''returns depth based on max cover'''
+    max_cover = float(df.loc[row,"COVER"].split('max-')[1])
+    return max_cover
+
+def ol2dl(lst, att):
+    a_set = set([getattr(obj, att) for obj in lst])
+    return dict((key, []) for key in a_set)
+
+
+filename = askopenfilename(title = "Select PIPE file",filetypes = (('CSV Files', '*.csv'),))
+df = pd.read_csv(filename)
+df = delete_level(df)
+df['Diameter'] = df['Diameter'].str.replace('cm','')
+df['Diameter'] = pd.to_numeric(df['Diameter'])
+df['Length'] = pd.to_numeric(df['Length'])
+df_ex = df[['Pipe Name', 'Diameter' , 'Length']]
+df_ex['Depth'] = np.nan
+pipelst = []
+
+for row in range(len(df)):
+    df.loc[row,'COVER'] = show_only_max_cover(df, row)
+    name = df.loc[row,'Pipe Name']
+    dia = df.loc[row,'Diameter']
+    length = df.loc[row,'Length']
+    pipelst.append(Pipe(name,dia,length))
+    pipelst[row].depth = float(df.loc[row,'COVER']) + ((float(pipelst[row].diameter))/100) + 2*(float(pipelst[row].twidth)/100)
+    df_ex.loc[row,'Depth'] = pipelst[row].depth 
+
+qp = ol2dl(pipelst, 'diameter')
+
+for dia in qp:
+    for d in range(1,8):
+        total = 0
+        for pipe in pipelst:
+            counter = 0
+            if d == 1:
+                if pipe.depth <= 2 and pipe.diameter == dia:
+                    total +=  float("{:.2f}".format(pipe.length))
+                    
+            elif (pipe.depth > d and pipe.depth <= d+1) and (pipe.diameter == dia):
+                total += float("{:.2f}".format(pipe.length))  
+            
+
+            
+       
+        qp[dia].append(total)
+
+
+row_index = ["0-2","2-3","3-4","4-5","5-6","6-7","7-8"]
+df_ex_sheet_2 = pd.DataFrame(data = qp, index = row_index)
+df_ex_sheet_2 = df_ex_sheet_2.reindex(sorted(df_ex_sheet_2.columns), axis = 1)
+with pd.ExcelWriter(filename.split('.csv')[0] + '-output.xlsx') as writer:
+        df_ex.to_excel(writer, sheet_name="מקור")
+        df_ex_sheet_2.to_excel(writer, sheet_name="כמויות")
+
+print("All Done!")
+
+
+
+
+
+
+
---
+++ Drainage Structures - MAAZ.py
@@ -0,0 +1,139 @@
+#########################################Definitions#############################################
+#Manholes with attributes of size, name and depths
+class Structure:
+    
+    def __init__(self, name, size, depth):
+        self.name = name
+        self.size = size
+        self.depth = depth
+    
+    def is_receptor(self):
+        if self.size == "76X37" or self.size == "37X76":
+            return True
+        return False
+
+
+def dimenstion(df,row):
+    #Cleans size text
+    dim = df.loc[row, "SIZE"]  
+    match = re.search(r'\d+X\d+|(?<=D- )\d+',dim)    #(?<=x) means find what x proceeds
+    if match != None:
+        return match.group()
+    else:
+        return "מוצא ניקוז" 
+
+def transpose_size(df,row):
+    # prevents sizes of 120X140 and 140X120 to count as 2 diffrent sizes 
+    size = df.loc[row,"SIZE"]
+    match = re.search(r'\d+X\d+', size)
+    if match!= None:
+        sp = size.split('X')
+        return str(min(sp)) + 'X' + str(max(sp))
+    else:
+        return size
+
+
+def convert_to_object_list(df,row):
+        name = df.loc[row, "Name"]
+        size = df.loc[row, "SIZE"]
+        depth = df.loc[row, "DEPTH"]
+        return (name, size, depth)
+
+
+
+#adds to a list of structures 
+def depth_counter(lst):
+    pass
+
+#Object list to dictionary of one of the attributes as unique keys and an empty list as value
+def ol2dl(lst, att):
+    a_set = set([getattr(obj, att) for obj in lst if '???' not in getattr(obj, att)])
+    return dict((key, []) for key in a_set)
+    
+
+###################################################Main Code############################################################
+if __name__ == "__main__":
+    import pandas as pd
+    import re
+    from tkinter.filedialog import askopenfilename
+    from tkinter import filedialog
+    import numpy as np
+
+    filename = askopenfilename(title = "Select STRUCTURE file", filetypes = (('CSV files', '*.csv'),))
+
+    df = pd.read_csv(filename,)
+    df_mh = df
+    df_mh.columns = df_mh.iloc[0]           ##Makes the second row the header
+    df_mh = df_mh.drop(0)
+    df_mh = df_mh.drop("Details",1) 
+    df_mh = df_mh.where(df_mh != '???' , 0)  ##replaces '???' from the depths with 0
+
+
+    for row in range(1,len(df_mh)+1):
+        check_numeric = re.search(r'\d', df_mh.loc[row,"SIZE"])
+        if check_numeric == None:
+            df_mh.loc[row,"SIZE"] = "0"
+        df_mh.loc[row, "SIZE"] = dimenstion(df_mh,row)
+        df_mh.loc[row, "SIZE"] = transpose_size(df_mh,row)
+
+
+    df = df_mh
+
+
+    # Structure quantification
+    s_list = []
+
+    for row in range(1,len(df)+1):
+        name,size,depth = convert_to_object_list(df,row)
+        s_list.append(Structure(name,size,float(depth)))
+
+
+    qs = ol2dl(s_list, 'size') 
+    # ^#qs for quantities structure
+    # Creates a dictionary with keys of sizes and values as as an empty list of counters where:
+    # index 0 is depth 0-2
+    # index 1 is depth 2-3
+    # index 2 is depth 3-4
+    # etc.
+
+    for key in qs:
+        # First we check in range of 0-2 or 0-1.5 
+        counter = 0
+        for obj in s_list:
+                if (obj.size == key) and (obj.depth > 0 and obj.depth <= 1.5):
+                    counter += 1
+        qs[key].append(counter)
+        # Then we check in range of 2-8 with increment of 0.5.
+        # for d in np.arange(1.25,7.75,0.5):
+        for d in np.arange(1.5,8,0.5):
+            counter = 0
+            for obj in s_list:
+                if (obj.size == key) and (obj.depth > d and obj.depth <= d+0.5):
+                    counter += 1
+            qs[key].append(counter)
+
+
+    # rowindex = ['0-1.25','1.25-1.75','1.75-2.25','2.25-2.75','2.75-3.25',
+    #              '3.25-3.75','3.75-4.25','4.25-4.75','4.75-5.25','5.25-5.75',
+    #              '5.75-6.25','6.25-6.75','6.75-7.25','7.25-7.75']
+    rowindex = ['0-1.5','1.5-2','2-2.5','2.5-3','3-3.5',
+                '3.5-4','4-4.5','4.5-5','5-5.5','5.5-6',
+                '6-6.5','6.5-7','7-7.5','7.5-8']
+
+    df_new = pd.DataFrame(data=qs,
+                            index=rowindex)
+
+   
+
+    with pd.ExcelWriter(filename.split('.csv')[0] + '-output.xlsx') as writer:
+        df.to_excel(writer, sheet_name="מקור")
+        df_new.to_excel(writer, sheet_name="כמויות")
+
+
+    print('All Done!')
+            
+
+
+
+
+
